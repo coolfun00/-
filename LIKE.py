2c1bd8541910f991a3914beea76b904167ad14f9
Youngju Je <68914587+jyjnluna@users.noreply.github.com>
2022-01-24 17:54:04 +0900

    파이썬 turtle 모듈 사용하기
    파이썬 turtle 모듈을 사용해서 LIKE라는 글자를 적어봤습니다.

---
+++ LIKE.py
@@ -0,0 +1,80 @@
+import turtle as t
+t.pencolor("blue")
+t.shape("turtle")
+t.speed(10)
+t.pensize(20)
+
+#L
+t.penup()
+t.goto(-225, 50)
+t.pendown()
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.penup()
+
+#I
+t.goto(-50, 50)
+t.pendown()
+t.right(90)
+t.forward(100)
+t.penup()
+
+#K
+t.goto(25, 50)
+t.pendown()
+t.forward(100)
+t.penup()
+t.goto(25, 0)
+t.pendown()
+t.left(45)
+t.forward(70)
+t.penup()
+t.goto(25, 0)
+t.pendown()
+t.left(90)
+t.forward(70)
+t.penup()
+
+#E
+t.goto(150, 50)
+t.pendown()
+t.right(135)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.penup()
+t.goto(150, 0)
+t.pendown()
+t.forward(100)
+t.penup()
+t.goto(150, 50)
+t.pendown()
+t.forward(100)
+t.penup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
